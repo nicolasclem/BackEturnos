f6cd4dfa529675c32f8783efd3e25153f5998575
Nicolas Clementi <clementinicolas@gmail.com>
2022-04-18 20:11:42 -0300

    dejo corriendo el server puerto

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express=require('express')
+const app=express()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const port=3003
+const path = require('path');
+const cors= require('cors')
+const cookies = require ('cookie-parser');
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(cors())// politica de seguirdad
 
-module.exports = app;
+app.use(
+    express.static(path.resolve(__dirname, '../public'))
+);// rutas partiendo desde / public
+
+
+app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+// nuestro motor de plantillas
+// app.set('view engine', 'ejs');
+// app.set('views', path.resolve(__dirname, 'views'));
+
+
+app.use(cookies());
+
+// middle de aplicacion
+
+
+
+
+
+
+app.listen(port, ()=>console.log(` servidor corriendo en ${port} `) )
