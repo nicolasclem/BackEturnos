ead5a40aa576e4203a809d89cddafc12b89a5005
Juan Campos <juanlad1221@gmail.com>
2022-04-19 09:07:33 -0300

    bd online y controladores nuevos

--- Controller/loginController.js
+++ Controller/loginController.js
@@ -8,10 +8,16 @@ module.exports = login = async (req, res) => {
 
     try {
         
-
         const [results] = await sequelize.query("SELECT * FROM users")
-
+        //const [check] = await sequelize.query("SELECT COUNT(email) AS count FROM users WHERE email = 'juanse@gmail.com' and password = '1234'")
+        //console.log(check)
+        /*if(check.count === 1){
+            res.status(200).json(check)
+        }else{
+            res.status(401).json({msg:'Not authorized...'})
+        }*/
         res.status(200).json(results)
+        
     } catch (error) {
         console.log(error)
     }
